47dd2c35518b406b0d3bd57edd228693805f5cf5
Kelly Robinson <krobinson6@kelly-robinsons-macbook-pro.local>
2009-08-04 19:22:09 -0700

    initial commit

---
+++ griffon-app/controllers/GriffonDemoController.groovy
@@ -0,0 +1,14 @@
+class GriffonDemoController {
+    // these will be injected by Griffon
+    def model
+    def view
+
+    void mvcGroupInit(Map args) {
+        // this method is called after model and view are injected. It's also been modified!
+    }
+
+    /*
+    def action = { evt = null ->
+    }
+    */
+}
